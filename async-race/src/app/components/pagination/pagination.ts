2c60db6618e034150634cfa2d2da4003b64cb980
Izy <126877709+izy-code@users.noreply.github.com>
2024-03-27 18:06:47 +0300

    feat: add pagination module

---
+++ async-race/src/app/components/pagination/pagination.ts
@@ -0,0 +1,41 @@
+import BaseComponent from '@/app/components/base-component';
+import { p } from '@/app/components/tags';
+import type Router from '@/app/router/router';
+import ButtonComponent from '../button/button';
+import CustomEventName from '@/app/events';
+import type EventEmitter from '@/app/utils/event-emitter';
+import type { Handler } from '@/app/utils/event-emitter';
+
+export default class PaginationComponent extends BaseComponent<HTMLDivElement> {
+  private text: BaseComponent<HTMLParagraphElement>;
+
+  constructor(router: Router, emitter: EventEmitter, pageNumber: number) {
+    super({ className: 'garage-page__pagination', tag: 'div' });
+
+    this.text = p({ className: 'garage-page__page-number', textContent: `Page #${pageNumber}` });
+
+    const nextPageButton = ButtonComponent({
+      className: 'garage-page__button-next button',
+      textContent: 'Next page',
+      buttonType: 'button',
+    });
+    const prevPageButton = ButtonComponent({
+      className: 'garage-page__button-prev button',
+      textContent: 'Prev page',
+      buttonType: 'button',
+    });
+
+    emitter.on(CustomEventName.PAGE_UPDATE, this.setPageNumber);
+
+    nextPageButton.addListener('click', () => emitter.emit(CustomEventName.NEXT_PAGE_SWITCH));
+    prevPageButton.addListener('click', () => emitter.emit(CustomEventName.PREV_PAGE_SWITCH));
+
+    this.appendChildren([this.text, nextPageButton, prevPageButton]);
+  }
+
+  private setPageNumber: Handler = (details): void => {
+    if (details && typeof details.pageNumber === 'number') {
+      this.text.setTextContent(`Page #${details.pageNumber}`);
+    }
+  };
+}
