8d70e448b1819de781049b3c9e357292606fc016
Izy <126877709+izy-code@users.noreply.github.com>
2024-03-31 12:14:57 +0300

    fix: add custom event names

--- async-race/src/app/events/index.ts
+++ async-race/src/app/events/index.ts
@@ -1,13 +1,30 @@
 enum CustomEventName {
-  NEXT_PAGE_SWITCH = 'nextPageSwitch',
-  PREV_PAGE_SWITCH = 'prevPageSwitch',
+  NEXT_PAGE_CLICK = 'nextPageSwitch',
+  PREV_PAGE_CLICK = 'prevPageSwitch',
+  CAR_CREATION_CLICK = 'carCreation',
+  CAR_SELECTION_CLICK = 'carSelection',
+  CAR_UPDATE_CLICK = 'carUpdate',
+  RACE_START_CLICK = 'raceStart',
+  RACE_RESET_CLICK = 'raceReset',
+  CARS_GENERATION_CLICK = 'carsGeneration',
+  CAR_START_CLICK = 'carStart',
+  CAR_RESET_CLICK = 'carReset',
+  CAR_REMOVE_CLICK = 'carRemove',
   PAGE_UPDATE = 'pageUpdate',
-  CAR_CREATION = 'carCreation',
-  CAR_SELECTION = 'carSelection',
-  CAR_UPDATE = 'carUpdate',
-  RACE_START = 'raceStart',
-  RACE_RESET = 'raceReset',
-  CARS_GENERATE = 'carsGenerate',
+  TRACKS_REFILL = 'tracksRefill',
+  ALL_CARS_START = 'allCarsStart',
+  ALL_CARS_RESET = 'allCarsReset',
+  CARS_UPDATE = 'carsUpdate',
+  WINNER_UPDATE = 'winnerUpdate',
+  MODAL_SHOW = 'modalShow',
+
+  CAR_MOVEMENT_VIEW = 'carMovementView',
+  CAR_STOP_VIEW = 'carStopView',
+  CAR_RESET_VIEW = 'carResetView',
+  CAR_SELECTION_VIEW = 'carSelectionView',
+
+  GARAGE_EDIT_DISABLED = 'garageEditDisabled',
+  GARAGE_EDIT_ENABLED = 'garageEditEnabled',
 }
 
 export default CustomEventName;
