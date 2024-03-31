fd3b67cea1b20d68894e07bf80f6f6c2edb84489
Izy <126877709+izy-code@users.noreply.github.com>
2024-03-31 12:21:03 +0300

    feat: add main block components

--- async-race/src/app/pages/garage/main/garage-main.ts
+++ async-race/src/app/pages/garage/main/garage-main.ts
@@ -7,25 +7,37 @@ import EventEmitter from '@/app/utils/event-emitter';
 import CarCreationComponent from './creation/car-creation';
 import CarUpdateComponent from './update/car-update';
 import RaceButtonsComponent from './race-buttons/race-buttons';
+import GarageState from '@/app/states/garage-state';
+import GarageController from '@/app/controllers/garage-controller';
+import RaceTracksComponent from './race-tracks/race-tracks';
+import ModalComponent from './modal/modal';
 
 export default class GarageMainComponent extends BaseComponent {
   private router: Router;
 
   private emitter: EventEmitter;
 
-  constructor(router: Router, pageNumber: number) {
+  private controller: GarageController;
+
+  constructor(router: Router) {
     super({ className: 'garage-page__main main', tag: 'main' });
 
     this.router = router;
     this.emitter = new EventEmitter();
+
+    const garageState = new GarageState();
+
+    this.controller = new GarageController(this.emitter, garageState);
 
     const pageName = Page.GARAGE.charAt(0).toUpperCase() + Page.GARAGE.slice(1);
     const pageInfo = new PageInfoComponent(this.emitter, pageName);
     const carCreation = new CarCreationComponent(this.emitter);
     const carUpdate = new CarUpdateComponent(this.emitter);
     const raceButtons = new RaceButtonsComponent(this.emitter);
-    const pagination = new PaginationComponent(this.router, this.emitter, pageNumber);
+    const pagination = new PaginationComponent(this.router, this.emitter);
+    const raceTracks = new RaceTracksComponent(this.emitter);
+    const modal = new ModalComponent(this.emitter);
 
-    this.appendChildren([pageInfo, carCreation, carUpdate, raceButtons, pagination]);
+    this.appendChildren([pageInfo, carCreation, carUpdate, raceButtons, pagination, raceTracks, modal]);
   }
 }
