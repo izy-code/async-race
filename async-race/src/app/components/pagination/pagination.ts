54896c6d73bd93f60a8714c62ad90fc26be3f048
Izy <126877709+izy-code@users.noreply.github.com>
2024-04-01 16:56:56 +0300

    feat: add buttons disable conditions

--- async-race/src/app/components/pagination/pagination.ts
+++ async-race/src/app/components/pagination/pagination.ts
@@ -1,7 +1,6 @@
 import './pagination.scss';
 import BaseComponent from '@/app/components/base-component';
 import { p } from '@/app/components/tags';
-import type Router from '@/app/router/router';
 import ButtonComponent from '../button/button';
 import CustomEventName from '@/app/events';
 import type EventEmitter from '@/app/utils/event-emitter';
@@ -9,31 +8,61 @@ import type EventEmitter from '@/app/utils/event-emitter';
 export default class PaginationComponent extends BaseComponent<HTMLDivElement> {
   private text: BaseComponent<HTMLParagraphElement>;
 
-  constructor(router: Router, emitter: EventEmitter) {
+  private nextPageButton: BaseComponent<HTMLButtonElement>;
+
+  private prevPageButton: BaseComponent<HTMLButtonElement>;
+
+  private isNextButtonDisabled = false;
+
+  private isPrevButtonDisabled = false;
+
+  constructor(emitter: EventEmitter) {
     super({ className: 'pagination', tag: 'div' });
 
     this.text = p({ className: 'pagination__page-number', textContent: 'Page #' });
-
-    const nextPageButton = ButtonComponent({
+    this.nextPageButton = ButtonComponent({
       className: 'pagination__button-next button',
       textContent: 'Next page',
       buttonType: 'button',
     });
-    const prevPageButton = ButtonComponent({
+    this.prevPageButton = ButtonComponent({
       className: 'pagination__button-prev button',
       textContent: 'Prev page',
       buttonType: 'button',
     });
 
-    emitter.on(CustomEventName.PAGE_UPDATE, this.setPageNumber);
+    emitter.on(CustomEventName.PAGE_UPDATE, this.onPageUpdate);
+    emitter.on(CustomEventName.RACE_START_CLICK, () => {
+      this.nextPageButton.setAttribute('disabled', '');
+      this.prevPageButton.setAttribute('disabled', '');
+    });
+    emitter.on(CustomEventName.RACE_RESET_CLICK, () => {
+      this.nextPageButton.getNode().disabled = this.isNextButtonDisabled;
+      this.prevPageButton.getNode().disabled = this.isNextButtonDisabled;
+    });
 
-    nextPageButton.addListener('click', () => emitter.emit(CustomEventName.NEXT_PAGE_CLICK));
-    prevPageButton.addListener('click', () => emitter.emit(CustomEventName.PREV_PAGE_CLICK));
+    this.nextPageButton.addListener('click', () => emitter.emit(CustomEventName.NEXT_PAGE_CLICK));
+    this.prevPageButton.addListener('click', () => emitter.emit(CustomEventName.PREV_PAGE_CLICK));
 
-    this.appendChildren([this.text, prevPageButton, nextPageButton]);
+    this.appendChildren([this.text, this.prevPageButton, this.nextPageButton]);
   }
 
-  private setPageNumber = (details: { pageNumber: number }): void => {
+  private onPageUpdate = (details: { pageNumber: number; pageCount: number }): void => {
     this.text.setTextContent(`Page #${details.pageNumber}`);
+
+    if (details.pageNumber === 1) {
+      this.prevPageButton.setAttribute('disabled', '');
+      this.isPrevButtonDisabled = true;
+    } else {
+      this.prevPageButton.removeAttribute('disabled');
+      this.isPrevButtonDisabled = false;
+    }
+    if (details.pageNumber === details.pageCount) {
+      this.nextPageButton.setAttribute('disabled', '');
+      this.isNextButtonDisabled = true;
+    } else {
+      this.nextPageButton.removeAttribute('disabled');
+      this.isNextButtonDisabled = false;
+    }
   };
 }
